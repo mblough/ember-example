e875041f6edebe27c80a4aa1dc93b4ec415d2070
mblough <mblough7@gmail.com>
2015-01-25 13:56:28 -0500

    Finishing touches on login and page access rights

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+	this.route('application');
   this.resource('contacts', function() {
       this.resource('contact', { path: '/:contact_id' });
     });
