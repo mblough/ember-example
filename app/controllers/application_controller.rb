08ed95b08a592a67f78dde5e4a9a466adac47ade
mblough <mblough7@gmail.com>
2015-01-25 11:58:56 -0500

    Disable serverside sessions and cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 	before_filter :authenticate_user_from_token!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   private
   	def authenticate_user_from_token!
